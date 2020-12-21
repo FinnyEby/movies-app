8043875ed1afb8d3e7f33c0cc042459202c47023
username <finnyebby@gmail.com>
2020-12-21 20:18:52 +0530

    Added Tabs inside modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,12 +3,26 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const customStyle = {
+    content: {
+        top : '50%',
+        left : '50%',
+        right : 'auto',
+        bottom : 'auto',
+        marginRight : '-50%',
+        transform : 'translate(-50%, -50%)'
+    }
+}
 
 class Header extends Component {
     constructor() {
         super();
         this.state = {
-            modalIsOepn : false
+            modalIsOepn : false,
+            value : 0
         };
     }
 
@@ -20,6 +34,10 @@ class Header extends Component {
         this.setState({modalIsOepn : false})
     }
 
+    tabChangeHandler = (event, value) => {
+        this.setState({value})
+    }
+
     render() {
         return(
             <div>
@@ -29,8 +47,15 @@ class Header extends Component {
                         <Button variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOepn} contentLabel="Login" onRequestClose={this.closeModalHandler}>
-                    
+                <Modal ariaHideApp={false} 
+                isOpen={this.state.modalIsOepn}
+                contentLabel="Login" 
+                onRequestClose={this.closeModalHandler}
+                style={customStyle}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login"/>
+                        <Tab label="Register"/>
+                    </Tabs>
                 </Modal>
             </div>
         )
