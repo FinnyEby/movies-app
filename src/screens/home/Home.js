e97bf985a06aa89a4abb327d582b263c160db133
username <finnyebby@gmail.com>
2020-12-23 00:04:19 +0530

    Added 'Movie Name' filter on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,6 +6,12 @@ import moviesData from '../../common/movieData';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 
 const styles = theme => ({
     root: {
@@ -22,10 +28,27 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
-    }
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+     },
+     title: {
+        color: theme.palette.primary.light,
+     }
 });
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movieName: ""
+        }
+    }
+    movieNameChangeHandler = (e) => {
+        this.setState({movieName : e.target.value})
+    }
     render() {
         const { classes } = this.props;
         return(
@@ -42,15 +65,32 @@ class Home extends Component {
                         </GridListTile>
                     ))}
                 </GridList>
-                <div className="movies-list">
-                    <GridList cols={3} spacing={25} cellHeight={'auto'}>
-                        {moviesData.map(movie => (
-                            <GridListTile key={movie.key} className="movies">
-                                <img src={movie.poster_url} />
-                                <GridListTileBar title={movie.title} />
-                            </GridListTile>
-                        ))}
-                    </GridList>
+                <div className="main-comtainer">
+                    <div className="movies-list">
+                        <GridList cols={3} spacing={25} cellHeight={'auto'}>
+                            {moviesData.map(movie => (
+                                <GridListTile key={movie.id} className="movies">
+                                    <img src={movie.poster_url} />
+                                    <GridListTileBar title={movie.title} subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}/>
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="filter">
+                        <Card>
+                            <CardContent>
+                                <FormControl className={classes.formControl}>
+                                    <Typography className={classes.Typography} color="textSecondary">
+                                        FIND MOVIES BY:
+                                    </Typography>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                                    <Input id="movieName" type="text" onChange={this.movieNameChangeHandler} />
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
             </div>
         )
