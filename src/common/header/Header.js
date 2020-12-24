727e523a0027027eb533afaeb0fa27255092c97b
username <finnyebby@gmail.com>
2020-12-24 23:13:13 +0530

    Added 'BookShow' component and Book Show button on header on details page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
@@ -11,6 +12,7 @@ import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow';
 
 const customStyle = {
     content: {
@@ -126,6 +128,10 @@ class Header extends Component {
         this.setState({contactno : e.target.value})
     }
 
+    bookShowHandler = () => {
+        ReactDOM.render(<BookShow />, document.getElementById('root'));
+    }
+
     render() {
         return(
             <div>
@@ -134,6 +140,9 @@ class Header extends Component {
                     <div className="login-button">
                         <Button variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
                     </div>
+                    {this.props.showBookShowButton === "true" ? <div className="book-show-button">
+                        <Button variant="contained" color="primary" onClick={this.bookShowHandler}>BOOK SHOW</Button>
+                    </div> : ""}
                 </header>
                 <Modal ariaHideApp={false} 
                 isOpen={this.state.modalIsOepn}
---
+++ src/screens/bookshow/BookShow.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Header from '../../common/header/Header';
+
+class BookShow extends Component {
+    render() {
+        return(
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+export default BookShow;
